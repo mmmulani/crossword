4e6dfa089691e249471b044de968423d39774699
Mehdi Mulani <mehdi@fb.com>
2013-09-26 20:17:38 +0100

    Initial Commit

---
+++ Crossword/MMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MMAppDelegate.h
+//  Crossword
+//
+//  Created by Mehdi Mulani on 9/26/13.
+//  Copyright (c) 2013 Mehdi Mulani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Crossword/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Crossword
+//
+//  Created by Mehdi Mulani on 9/26/13.
+//  Copyright (c) 2013 Mehdi Mulani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MMAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([MMAppDelegate class]));
+  }
+}
